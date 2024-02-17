3fd66a7c2ce8704daf6b2e7fd2035f533ceb296c
ap766 <anipatra733@gmail.com>
2024-02-17 22:23:42 +0530

    changed image path

--- frontend/src/components/NavigationBar.jsx
+++ frontend/src/components/NavigationBar.jsx
@@ -1,6 +1,6 @@
 // NavigationBar.js
 import React from 'react';
-import logo from '/home/anisha7/Documents/coding/Nokia/FrontendSection/frontend/src/images/logo.jpg'
+import logo from './logo.jpg'
 const NavigationBar = () => {
   return (
     <div className="navbar">
