50ebbfc504acf7d3597a23c33d0eea467068295c
Abbashozefa <abbashozefa@gmail.com>
2024-03-06 12:38:25 +0530

    Changes in DEployer works fine

--- AggregatorApi.py
+++ AggregatorApi.py
@@ -7,33 +7,37 @@ app=Flask(__name__)
 
 @app.route("/aggregate",methods=['POST'])
 def aggregate():
-    names=request.get_json()
-    current_dir = os.getcwd()
-    os.system('helm create '+names["project_name"])
-    charts_path=os.path.join(current_dir,names["project_name"],"charts")
-    os.chdir(charts_path)
-    for i in names["charts"]:
-        os.system("helm pull oci://registry-1.docker.io/dsanokia/"+i+" --untar")
+    try:
 
-        values_yaml_path = os.path.join(current_dir,names["project_name"],"charts", i, 'values.yaml')
-
-        # Read the values.yaml file
-        with open(values_yaml_path, 'r') as file:
-            yaml_data = yaml.safe_load(file)
+        names=request.get_json()
+        current_dir = os.getcwd()
+        os.system('helm create '+names["project_name"])
+        charts_path=os.path.join(current_dir,names["project_name"],"charts")
+        os.chdir(charts_path)
+        for i in names["charts"]:
+            os.system("helm pull oci://registry-1.docker.io/dsanokia/"+i+" --untar")
     
-        # Update the replica number in the YAML data
-        yaml_data['replicaCount'] = names["charts"][i]
+            values_yaml_path = os.path.join(current_dir,names["project_name"],"charts", i, 'values.yaml')
     
-        # Write the updated YAML data back to the file
-        with open(values_yaml_path, 'w') as file:
-            yaml.dump(yaml_data, file)
-    os.chdir(current_dir)
-
-    os.system("helm package "+names["project_name"])
-    os.system("helm push "+names["project_name"]+"-0.1.0.tgz oci://registry-1.docker.io/dsanokia/")
+            # Read the values.yaml file
+            with open(values_yaml_path, 'r') as file:
+                yaml_data = yaml.safe_load(file)
+        
+            # Update the replica number in the YAML data
+            yaml_data['replicaCount'] = names["charts"][i]
+        
+            # Write the updated YAML data back to the file
+            with open(values_yaml_path, 'w') as file:
+                yaml.dump(yaml_data, file)
+        os.chdir(current_dir)
     
-    print(os.getcwd())
-    return jsonify({"Creation":"Successful"}),200
+        os.system("helm package "+names["project_name"])
+        os.system("helm push "+names["project_name"]+"-0.1.0.tgz oci://registry-1.docker.io/dsanokia/")
+        
+        print(os.getcwd())
+        return jsonify({"Creation":"Successful"}),200
+    except:
+        return jsonify({"Creation":"Build Error"}),500
 if __name__=='__main__':
     app.run(port=5001)
 
--- DeploymentApi.py
+++ DeploymentApi.py
@@ -7,11 +7,17 @@ app=Flask(__name__)
 
 @app.route('/deployinprod',methods=['POST'])
 def deploy():
-    proj=request.get_json()
-    os.system('helm install '+proj['project_name']+' --version 0.1.0 oci://registry-1.docker.io/dsanokia/'+proj['project_name'])
+    try:
 
-    return jsonify({'Result':'Deployed Successfully'})
+        proj=request.get_json()
+        cmd=os.system('helm install '+proj['project_name']+' --version 0.1.0 oci://registry-1.docker.io/dsanokia/'+proj['project_name'])
+        if cmd==0:
+            return jsonify({'Result':'Deployed Successfully'})
+        else:
+            return jsonify({'Result':'Deployment Failure'})
+    except:
 
+        return jsonify({'Result':'Deployment Failure'})
 
 
 
