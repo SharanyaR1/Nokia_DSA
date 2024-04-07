4990b74f1eb8fbeae2bcfb9984bbb9dd4cc70217
Abbashozefa <abbashozefa@gmail.com>
2024-04-08 04:20:23 +0530

    Testing

--- DeploymentApi.py
+++ DeploymentApi.py
@@ -1,8 +1,32 @@
 from flask import Flask,request,jsonify
 import os
 import shutil
+import subprocess
 
 app=Flask(__name__)
+
+
+def check_pods_status(helm_release):
+    try:
+        # Run kubectl get pods command for the specified Helm release
+        while True:
+
+            cmd = f'kubectl get pods --namespace default -l app.kubernetes.io/instance={helm_release} -o jsonpath="{{.items[*].status.phase}}"'
+            result = subprocess.run(cmd, shell=True, capture_output=True, text=True, check=True)
+            print(result)
+            # Check if all pods are in the "Running" state
+            if set(result.stdout.split())== {'Running'} and len(set(result.stdout.split()))==1: 
+                print("Successful")
+                return True
+            #  and len(set(result.stdout.split()))!=1
+            print("Deploying..")
+        
+
+    except subprocess.CalledProcessError as e:
+        print(f"Error checking pods status: {e}")
+        return False
+
+
 
 
 @app.route('/deployinprod',methods=['POST'])
@@ -12,7 +36,16 @@ def deploy():
         proj=request.get_json()
         cmd=os.system('helm install '+proj['project_name']+' --version 0.1.0 oci://registry-1.docker.io/dsanokia/'+proj['project_name'])
         if cmd==0:
-            return jsonify({'Result':'Deployed Successfully'})
+            # helm_release = 'uness'
+            # The release name of helm in helm install command 
+            helm_release = proj['project_name']
+            if check_pods_status(helm_release):
+                print(f"All pods in Helm deployment '{helm_release}' are running.")
+                return jsonify({'Result':'Deployed Successfully'})
+            else:
+                print(f"Not all pods in Helm deployment '{helm_release}' are running.")
+                return jsonify({'Result':'Deployment Failure'})
+            
         else:
             return jsonify({'Result':'Deployment Failure'})
     except:
