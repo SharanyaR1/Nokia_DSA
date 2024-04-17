71052f57ace62d2a91083d545e504996ca59b358
Abbashozefa <abbashozefa@gmail.com>
2024-04-17 20:30:22 +0530

    Update

--- DeploymentApi.py
+++ DeploymentApi.py
@@ -3,7 +3,37 @@ import os
 import shutil
 import subprocess
 import json
+from logging.config import dictConfig
 app=Flask(__name__)
+
+#THIS IS FOR LOGGING IT WRITES THE LOGS TO A FILE CALLED deployment.log
+dictConfig(
+    {
+        "version": 1,
+        "formatters": {
+            "default": {
+                "format": "[%(asctime)s] %(levelname)s in %(module)s: %(message)s",
+            }
+        },
+        "handlers": {
+            "console": {
+                "class": "logging.StreamHandler",
+                "stream": "ext://sys.stdout",
+                "formatter": "default",
+            },
+            "file": {
+
+                "class": "logging.FileHandler",
+                "filename": "deployment.log",
+                "formatter": "default",
+
+            },
+           
+        },
+        
+        "root": {"level": "DEBUG", "handlers": ["console","file"]},
+    }
+)
 
 
 def check_pods_status(helm_release):
